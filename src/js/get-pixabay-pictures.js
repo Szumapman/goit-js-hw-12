7df2290eb056294a92a7d13e6e918e3aa163a993
Paweł Szumański <pawel.szumanski@outlook.com>
2024-12-30 17:46:46 +0100

    change form fetch to axios

--- src/js/get-pixabay-pictures.js
+++ src/js/get-pixabay-pictures.js
@@ -1,23 +1,23 @@
 "use strict"
 
+import axios from "axios";
+
 const pixabayApiKey = "47865988-0e202f3c070f6317e288e8f49"; // should be f.e. in .env file
 
-export function getPictures(searchTerm) {
-    const searchParams = new URLSearchParams({
-        key: pixabayApiKey,
-        q: searchTerm,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-        per_page: 15,
-    });
-
-    return fetch(`https://pixabay.com/api/?${searchParams}`)
+export function getPictures(searchTerm, page = 1, perPage = 40) {
+    return axios.get("https://pixabay.com/api/", {
+        params: {
+            key: pixabayApiKey,
+            q: searchTerm,
+            image_type: 'photo',
+            orientation: 'horizontal',
+            safesearch: true,
+            per_page: perPage,
+            page: page,
+        }
+    })
         .then(response => {
-            if (!response.ok) {
-                throw new Error(response.status);
-            }
-            return response.json();
+            return response.data;
         }).catch(error => {
             console.error(error);
         });
