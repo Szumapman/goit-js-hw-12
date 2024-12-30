e06386c26dfb55d95428861b8e7544836fe7eafe
Paweł Szumański <pawel.szumanski@outlook.com>
2024-12-30 18:15:51 +0100

    change function getPictures to async function

--- src/js/get-pixabay-pictures.js
+++ src/js/get-pixabay-pictures.js
@@ -4,21 +4,22 @@ import axios from "axios";
 
 const pixabayApiKey = "47865988-0e202f3c070f6317e288e8f49"; // should be f.e. in .env file
 
-export function getPictures(searchTerm, page = 1, perPage = 40) {
-    return axios.get("https://pixabay.com/api/", {
-        params: {
-            key: pixabayApiKey,
-            q: searchTerm,
-            image_type: 'photo',
-            orientation: 'horizontal',
-            safesearch: true,
-            per_page: perPage,
-            page: page,
-        }
-    })
-        .then(response => {
-            return response.data;
-        }).catch(error => {
-            console.error(error);
+export async function getPictures(searchTerm, page = 1, perPage = 40) {
+    axios.defaults.baseURL = "https://pixabay.com/api";
+    try {
+        const response = await axios.get("/", {
+            params: {
+                key: pixabayApiKey,
+                q: searchTerm,
+                image_type: 'photo',
+                orientation: 'horizontal',
+                safesearch: true,
+                per_page: perPage,
+                page: page,
+            }
         });
+        return response.data;
+    } catch (error) {
+        console.log("Error during getting images from Pixabay: ", error);
+    }
 }
